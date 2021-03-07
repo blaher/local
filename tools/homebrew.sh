8fb62b73cd2ebf67a054c88aee39ccb66b637170
Benjamin J. Young <blaher09@gmail.com>
2021-03-06 23:19:25 -0500

    Installing homebrew tools in install

--- tools/homebrew.sh
+++ tools/homebrew.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-echo "Installing homebrew..."
+echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing Homebrew tools..."
+for tool in ~/Projects/local/tools/homebrew/*
+do
+  /bin/bash "$tool"
+done
