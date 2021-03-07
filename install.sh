1cffe4486a9f0998a76d1f2b662fa5c7c6c74b4b
Benjamin J. Young <blaher09@gmail.com>
2021-03-06 23:02:30 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
+default_name="Benjamin J. Young"
 
 # Setup xcode
+echo "Setting up xcode..."
 xcode-select --install
 
 # Configure Git
-git config --global user.name "Benjamin J. Young"
-echo "Enter Email:"
-read email
+echo "Configuring git..."
+read -p "Enter your name [$default_name]: " name
+name=${name:-"$default_name"}
+git config --global user.name "$name"
+read -p "Enter your email: " name
 git config --global user.email "$email"
 
 # Setup Projects Folder
+echo "Setting up projects..."
 mkdir ~/Projects
 
 # Download Repo
-git clone https://github.com/blaher/local-environment.git
+git clone https://github.com/blaher/local.git ~/Projects/local
 
-# Install neeeded tools
-for tool in ~/Projects/local-environment/tools/*
+# Install needed tools
+echo "Installing tools..."
+for tool in ~/Projects/local/tools/*
 do
   /bin/bash "$tool"
 done
 
 # Link bash profile
-ln -s ~/Projects/local-environment/.bash_profile ~/.bash_profile
+echo "Finishing up..."
+ln -s ~/Projects/local/.bash_profile ~/.bash_profile
