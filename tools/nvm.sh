784f8ea5f6390dfce31d798f44ee540f4cc24324
Benjamin J. Young <blaher09@gmail.com>
2021-03-06 23:29:49 -0500

    Refreshing bash profile on nvm install

--- tools/nvm.sh
+++ tools/nvm.sh
@@ -3,5 +3,8 @@
 echo "Installing NVM..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
+echo "Refreshing bash profile..."
+source ~/.bash_profile
+
 echo "Installing Node..."
 nvm use node
