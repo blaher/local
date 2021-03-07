164e65a36b9f38d1839f205e276aed72caeb23b4
Benjamin J. Young <blaher09@gmail.com>
2021-03-06 23:11:41 -0500

    Asking for github username on install

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+default_github="blaher"
 default_name="Benjamin J. Young"
 
 # Setup xcode
@@ -7,6 +8,8 @@ xcode-select --install
 
 # Configure Git
 echo "Configuring git..."
+read -p "Enter your github username [$default_github]: " github
+github=${name:-"$default_github"}
 read -p "Enter your name [$default_name]: " name
 name=${name:-"$default_name"}
 git config --global user.name "$name"
@@ -18,7 +21,7 @@ echo "Setting up projects..."
 mkdir ~/Projects
 
 # Download Repo
-git clone git@github.com:blaher/local.git ~/Projects/local
+git clone git@github.com:${github}/local.git ~/Projects/local
 
 # Install needed tools
 echo "Installing tools..."
