27daf00be31680f54f8dafa278d04c8e6bb462e2
Benjamin J. Young <blaher09@gmail.com>
2021-03-06 22:36:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+xcode-select --install
+
+mkdir ~/Projects
+cd ~/Projects
+git clone git@github.com:blaher/local-environment.git
+cd local-environment
+
 for tool in /tools/*
 do
   /bin/bash "$tool"
