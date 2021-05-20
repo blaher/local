87f916db28003e3528aee55eebdf71f8590bdc80
Benjamin J. Young <ben@blaher.me>
2021-05-20 11:25:31 -0400

    Update gnupg.sh

--- tools/homebrew/gnupg.sh
+++ tools/homebrew/gnupg.sh
@@ -2,3 +2,7 @@
 
 echo "Installing GnuPG..."
 brew install gnupg
+gpg --full-generate-key
+gpg --list-secret-keys --keyid-format LONG
+read -p "Enter your GPG key from above: " gpgkey
+gpg --armor --export gpgkey
